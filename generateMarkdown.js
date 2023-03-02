649a96693bcc0f9b3006f14edbdddd5ed8965adb
stephen Batelaan <sbatelaan91@gmail.com>
2023-03-01 20:27:58 -0800

    final

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,55 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  //repeat if else for rest of license
+  //if none return string that says no license
+  if (license === 'APACHE2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  if (license === 'bsd-3-clause') {
+    return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+  }
+  else {
+  return 'No License'
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+  console.log(data)
+  return `
+  # ${data.title}
+  ## License
+  ${renderLicenseLink(data.license[0])}
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [How to Use This Application](#HowToUseThisApplication)
+  * [Contributors](#contributors)
+  * [Testing](#testing)
+  * [Questions](#questions)
+  ## Description
+  ${data.description}
+  ## Installation
+  ${data.installation}
+  ## How To Use Application
+  ${data.usage}
+  ## Testing
+  ${data.tests}
+  ## Contributors
+  ${data.contributors}
+  ## Application link
+  ${data.link}
+  # Questions
+  ## Contact me via Email
+  ${data.email}
+  ## Link to my Github profile
+  ${data.creator}`
 }
+
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -2,16 +2,76 @@
 
 const fs = require('fs');
 const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown')
+const path = require('path')
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+      name: 'title',
+      message: 'What is the name of your project?'},
+
+      {
+        type: 'input',
+      name: 'description',
+      message: 'Give a description of your project'},
+
+      {
+        type: 'input',
+      name: 'installation',
+      message: 'How does the user install/use the application?'},
+
+      {
+        type: 'input',
+      name: 'tests',
+      message: 'Give instructions on how to run all necessary tests'},
+
+      {
+        type: 'input',
+      name: 'usage',
+      message: 'How does the user use the application?'},
+
+      {
+        type: 'input',
+      name: 'contributors',
+      message: 'Who else contributed on the project? (Github username)'},
+
+      {
+      type: "checkbox",
+    name: "license",
+    message: "Please select a license applicable to this project.",
+    choices: ["MIT", "APACHE2.0", "bsd-3-clause", "none"],},
+
+        {
+        type: "input",
+    name: "creator",
+    message: "Link to your github",},
+
+        {
+        type: "input",
+    name: "email",
+    message: "Provide a valid email address",},
+
+          {
+    type: "input",
+    name: "link",
+    message: "Provide a link (URL) to deployed application",},
+
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  console.log(data)
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+function init() {
+    inquirer.prompt(questions).then((responses) => {
+        console.log("Creating Professional README...");
+        writeToFile("./README.md", generateMarkdown({...responses}));
+    })
+}
